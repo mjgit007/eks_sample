f575a36b033a809cb19684537572f20356afd109
mjgit007 <manjshet007@gmail.com>
2022-05-01 14:32:28 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "20.0.0.0/16"
+}
